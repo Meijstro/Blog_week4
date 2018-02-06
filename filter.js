7742645535034a5d27694d4942d908efd6ef5da4
Meijstro <35121706+Meijstro@users.noreply.github.com>
2018-02-06 14:57:43 +0100

    Add files via upload
    [W4-003] completed

--- filter.js
+++ filter.js
@@ -8,16 +8,29 @@ allbtn.addEventListener("click", function(){
 
   for(var i = 0; i < a.length; i++){
     a[i].style.display = "block";
+
+  };
+  for(var i = 0; i < b.length; i++){
     b[i].style.display = "block";
+
+  };
+  for(var i = 0; i < c.length; i++){
     c[i].style.display = "block";
     }
+
 });
 
 fcbtn.addEventListener("click", function(){
 
   for(var i = 0; i < a.length; i++){
     a[i].style.display = "block";
+
+  };
+  for(var i = 0; i < b.length; i++){
     b[i].style.display = "none";
+
+  };
+  for(var i = 0; i < c.length; i++){
     c[i].style.display = "none";
     }
 });
@@ -25,19 +38,31 @@ fcbtn.addEventListener("click", function(){
 
 cryptobtn.addEventListener("click", function(){
 
-  for(var i = 0; i < b.length; i++){
+  for(var i = 0; i < a.length; i++){
     a[i].style.display = "none";
+
+  };
+  for(var i = 0; i < b.length; i++){
     b[i].style.display = "block";
+
+  };
+  for(var i = 0; i < c.length; i++){
     c[i].style.display = "none";
-  }
+    }
 });
 
 trumpbtn.addEventListener("click", function(){
 
-  for(var i = 0; i < c.length; i++){
+  for(var i = 0; i < a.length; i++){
     a[i].style.display = "none";
+
+  };
+  for(var i = 0; i < b.length; i++){
     b[i].style.display = "none";
+
+  };
+  for(var i = 0; i < c.length; i++){
     c[i].style.display = "block";
-  }
+    }
 });
 }
